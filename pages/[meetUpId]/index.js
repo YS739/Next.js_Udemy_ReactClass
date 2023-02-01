783105968663652b2db3e52c04804fad8c261e75
YS739 <suk921@gmail.com>
2023-02-01 19:56:46 +0900

    Get dynamic data

--- pages/[meetUpId]/index.js
+++ pages/[meetUpId]/index.js
@@ -1,27 +1,35 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://user-images.githubusercontent.com/112805225/215753580-0c0f16df-fcc5-4e58-bcad-1baa42c14a2a.jpg"
-      title="Seville Plaza Meetup"
-      address="Seville plaza 1st"
-      description="This is a Tapas Tour!"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://test:${process.env.NEXT_PUBLIC_MONGODB_PW}@cluster0.uvn1uhq.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  // find의 첫번째 값은 가져오고 싶은 데이터, 모두 가져오려면 빈 배열
+  // 두 번째는
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: { meetupId: "m1" },
-      },
-      {
-        params: { meetupId: "m2" },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
@@ -29,17 +37,30 @@ export async function getStaticPaths() {
 // context로 id에 접근 가능
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
-  console.log("meetupId :>> ", meetupId);
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://test:${process.env.NEXT_PUBLIC_MONGODB_PW}@cluster0.uvn1uhq.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    // 강의에서는 new 없이 썼는데 new를 쓰라는 오류가 떠서 써줬더니 오류 해결
+    _id: new ObjectId(meetupId),
+  });
+  console.log("selectedMeetup :>> ", selectedMeetup);
+
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://user-images.githubusercontent.com/112805225/215753580-0c0f16df-fcc5-4e58-bcad-1baa42c14a2a.jpg",
-        id: meetupId,
-        title: "Seville Plaza Meetup",
-        address: "Seville plaza 1st",
-        description: "This is a Tapas Tour!",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
