510f46e1b5c82c0e5153caa1146e7307c5beaebb
YS739 <suk921@gmail.com>
2023-02-01 15:51:33 +0900

    Edit choose getStaticProps instead of getSergetServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,17 @@ const HomePage = (props) => {
   );
 };
 
+// 요청이 들어올 때마다 서버에 데이터 요청
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data form an API
+//   return {
+//     props: { meetups: DUMMY_MEETUPS },
+//   };
+// }
+
 // 오직 build할 때만 실행되는 코드
 export async function getStaticProps() {
   // fetch data from an API
@@ -34,7 +45,7 @@ export async function getStaticProps() {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 5,
+    revalidate: 1,
   };
 }
 
